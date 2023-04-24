c4e0aeb0364d09d32bf8eda70206f5cd166daed0
carznot2 <g.shaw@shaw.ca>
2023-04-24 14:24:08 -0600

    prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        while(true)
+        {
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = -1;
+            int counter = 0;
+            string answer = "";
+
+            {
+                Console.WriteLine("What is your guess (1-100)? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if(guess==magicNumber)
+                {
+                    Console.WriteLine("You guessed it!!");
+                }
+                else if(guess > magicNumber)
+                {
+                    Console.WriteLine("Guess a lower number.");
+                }
+                else
+                {
+                    Console.WriteLine("Guess a higher number.");
+                }
+                counter ++;
+            }
+
+            Console.WriteLine($"It took you {counter} guesses.");
+            while(answer.ToUpper() != "N" && answer.ToUpper() != "Y")
+            {
+                Console.WriteLine("Would you like to play again (Y/N)? ");
+                answer = Console.ReadLine();
+            }
+            if (answer.ToUpper() == "N")
+            {
+                break;
+            }
+        }
     }
 }
