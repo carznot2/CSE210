d9ee19b97cc8d704164d6b6b0d2426367cad6574
carznot2 <g.shaw@shaw.ca>
2023-05-04 11:11:03 -0600

    finish

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,5 +9,19 @@ class Program
         job1._JobTitle="Engineer";
         job1._startYear=2018;
         job1._endYear=2022;
+        // job1.Display();
+    
+        Job job2 = new Job();
+        job2._company="Apple";
+        job2._JobTitle="Software Engineer";
+        job2._startYear=2016;
+        job2._endYear=2018;
+        // job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name="George";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
